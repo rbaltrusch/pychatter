18d75c59b5784adecf7584e851835cfe02788e2c
Richard Baltrusch <richard@baltrusch.net>
2022-01-27 22:58:23 +0100

    Add socket cleanup

--- src/main.py
+++ src/main.py
@@ -4,9 +4,10 @@ Created on Mon May 10 00:12:31 2021
 
 @author: Korean_Crimson
 """
-
-from network import server, cleanup
-from gui import app, init
+from gui import app
+from gui import init
+from network import cleanup
+from network import server
 
 init.init()
 app.pack_all()
