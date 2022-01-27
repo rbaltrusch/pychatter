eabcd3a7f28f1bd8baedf1f3aa2069f6184dc057
Richard Baltrusch <richard@baltrusch.net>
2022-01-27 22:58:07 +0100

    Add socket cleanup

--- src/main.py
+++ src/main.py
@@ -5,23 +5,15 @@ Created on Mon May 10 00:12:31 2021
 @author: Korean_Crimson
 """
 
-import network
+from network import server, cleanup
 from gui import app, init
 
 init.init()
 app.pack_all()
-app.mainloop()
 
-#close socket connection
-try:
-    if app.data['connection']:
-        app.data['connection'].close()
-except:
-    pass
-
-#close server socket if hosting
-try:
-    if network.server.socket_:
-        network.server.socket_.close()
-except:
-    pass
+with cleanup.SocketCleanup(
+    lambda: app.data['connection'],
+    lambda: server.socket_
+):
+    app.mainloop()
+    server.kill()
