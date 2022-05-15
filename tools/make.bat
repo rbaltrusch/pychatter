17eb69083eec53a3391d966f089e04c2e9edda18
Richard Baltrusch <richard@baltrusch.net>
2022-05-15 16:23:20 +0200

    Fix make.bat for new package structure

--- tools/make.bat
+++ tools/make.bat
@@ -10,9 +10,9 @@ if exist "dist/main" (
 	rmdir /s /q "dist/main"
 )
 
-call "%pyinstaller_path%" --onedir --noupx --icon src/gui/media/icon.ico --exclude matplotlib --exclude pandas --exclude numpy src/main.py --noconsole
+call "%pyinstaller_path%" --onedir --noupx --icon localchat/gui/media/icon.ico --exclude matplotlib --exclude pandas --exclude numpy localchat/__main__.py --noconsole -n LocalChat
 
-echo v| xcopy /s /v /y "src/gui/media" "dist/main/gui/media"
+echo v| xcopy /s /v /y "localchat/gui/media" "dist/main/gui/media"
 call "tools/link.vbs" "dist/LocalChat.lnk" "dist/main/main.exe"
 copy "config.json" "dist"
 copy "README.md" "dist"
