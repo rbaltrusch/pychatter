7d98bcd4ee67ca0d67e70ea3d040895202f1d10b
Richard Baltrusch <richard@baltrusch.net>
2022-08-17 15:39:51 +0200

    Fix client tests reusing port for multiple tests

--- tests/unit/network/client_test.py
+++ tests/unit/network/client_test.py
@@ -8,17 +8,16 @@ import threading
 import pytest
 from localchat.network import client, config, server, util
 
-port = None
-
 
 class ServerSocket:
     """Server socket. Runs the server on localhost"""
 
     def __init__(self):
         self.server_thread = None
+        self.port = random.randint(10000, 65535)
 
     def __enter__(self):
-        server.init(port=port)
+        server.init(port=self.port)
         self.server_thread = threading.Thread(target=server.run_forever, daemon=True)
         self.server_thread.start()
         return self
@@ -26,12 +25,6 @@ class ServerSocket:
     def __exit__(self, *_):
         server.socket_.close()
         server.kill()
-
-
-def setup():
-    global port  # pylint: disable=global-statement
-    port = random.randint(10000, 65535)
-    print("setup", port)
 
 
 def teardown():
@@ -42,20 +35,24 @@ def teardown():
 @pytest.mark.parametrize("ip_address", [util.get_host_ip()])
 def test_network_connection_fail(ip_address):
     with pytest.raises(client.StartConnectionError):
-        client.NetworkConnection(ip_address, port=port, timeout=0.2).connect()
+        client.NetworkConnection(
+            ip_address, port=ServerSocket().port, timeout=0.2
+        ).connect()
 
 
 @pytest.mark.parametrize("ip_address", ["127.0.0.2"])
 def test_wrong_server_ip(ip_address):
-    with ServerSocket():
+    with ServerSocket() as socket_:
         with pytest.raises(client.StartConnectionError):
-            client.NetworkConnection(ip_address, port=port, timeout=0.2).connect()
+            client.NetworkConnection(
+                ip_address, port=socket_.port, timeout=0.2
+            ).connect()
 
 
 @pytest.mark.parametrize("ip_address", [util.get_host_ip()])
 def test_connect(ip_address):
-    with ServerSocket():
-        conn = client.NetworkConnection(ip_address, port=port)
+    with ServerSocket() as socket_:
+        conn = client.NetworkConnection(ip_address, port=socket_.port)
         conn.connect()
         assert conn.id is not None
         assert bool(conn)
@@ -63,8 +60,9 @@ def test_connect(ip_address):
 
 @pytest.mark.parametrize("ip_address", [util.get_host_ip()])
 def test_send_fail(ip_address):
-    conn = client.NetworkConnection(ip_address, port=port, timeout=0.5)
-    with ServerSocket():
+    socket_ = ServerSocket()
+    conn = client.NetworkConnection(ip_address, port=socket_.port, timeout=0.5)
+    with socket_:
         conn.connect()
         response = conn.send("abc".encode())
     assert response is None
@@ -75,8 +73,9 @@ def test_send_fail(ip_address):
     [("abc", "", 400), ("get/clients", "", 200)],
 )
 def test_send_request(head, body, expected_response_status):
-    conn = client.NetworkConnection(ip_address=util.get_host_ip(), port=port)
-    with ServerSocket():
+    socket_ = ServerSocket()
+    conn = client.NetworkConnection(ip_address=util.get_host_ip(), port=socket_.port)
+    with socket_:
         conn.connect()
         response = conn.send_request(head=head, body=body)
     assert response.get("status") == expected_response_status
@@ -84,14 +83,15 @@ def test_send_request(head, body, expected_response_status):
 
 @pytest.mark.parametrize("ip_address", [util.get_host_ip()])
 def test_bool(ip_address):
-    conn = client.NetworkConnection(ip_address, port=port)
+    conn = client.NetworkConnection(ip_address, port=ServerSocket().port)
     assert not bool(conn)
 
 
 @pytest.mark.parametrize("ip_address", [util.get_host_ip()])
 def test_close(ip_address):
-    conn = client.NetworkConnection(ip_address, port=port, timeout=0.5)
-    with ServerSocket():
+    socket_ = ServerSocket()
+    conn = client.NetworkConnection(ip_address, port=socket_.port, timeout=0.5)
+    with socket_:
         conn.connect()
         conn.close()
     assert conn.id is None
