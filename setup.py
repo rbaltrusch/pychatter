459e10da2abc64d5e3e050e0808ebd77ccaad207
Richard Baltrusch <richard@baltrusch.net>
2023-10-08 14:09:31 +0200

    Add chat pypi trope (#16)
    * Add chat pypi trope
    
    * Update setup.py
    
    Add "Intended Audience :: End Users/Desktop" pypi trope

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setuptools.setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
@@ -38,6 +39,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Topic :: Communications :: Chat",
         "Typing :: Typed",
     ],
 )
